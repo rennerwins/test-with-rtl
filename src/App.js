5cee1faba81a3646fd9f94b0ffff5cbb5ad75529
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2019-03-10 21:43:36 +0700

    Push ep2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  state = {
+    name: 'React',
+    inputValue: '',
+  }
+
+  handleChange = e => {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  hanldeClick = () => {
+    this.setState({
+      name: this.state.inputValue,
+      inputValue: '',
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            {this.props.text || 'Learn React'}
-          </a>
+          <h1>Hello, {this.state.name}!</h1>
+          <input
+            type="text"
+            placeholder="Type here"
+            aria-label="name-field"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.hanldeClick}>Change Name</button>
         </header>
       </div>
     )
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import App from './App'
 
 afterEach(cleanup)
 
-describe('getByText', () => {
-  it('should render `Learn React`', () => {
-    const { getByText } = render(<App />)
-    getByText('Learn React')
+describe('fireEvent', () => {
+  it('should find value in input field', () => {
+    const { getByLabelText } = render(<App />)
+    const input = getByLabelText('name-field')
+    fireEvent.change(input, { target: { value: 'Win' } })
+    expect(input.value).toEqual('Win')
   })
 
-  it('should render new text with props change', () => {
-    const { getByText, rerender } = render(<App />)
-    getByText('Learn React')
-    rerender(<App text="Testing React" />)
-    getByText('Testing React')
+  it('should change message to `Win`', () => {
+    const { getByText, getByLabelText } = render(<App />)
+    const input = getByLabelText('name-field')
+    const button = getByText('Change Name')
+    fireEvent.change(input, { target: { value: 'Win' } })
+    fireEvent.click(button)
+    getByText('Hello, Win!')
+  })
+
+  it('should clear text input value when click button', () => {
+    const { getByText, getByLabelText } = render(<App />)
+    const input = getByLabelText('name-field')
+    const button = getByText('Change Name')
+    fireEvent.change(input, { target: { value: 'Win' } })
+    fireEvent.click(button)
+    expect(input.value).toEqual('')
   })
 })
